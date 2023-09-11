7f71d3cde232915a3acc8144a87f2bfbf7e9ce4a
danielcuellop <franciscocuello86@gmail.com>
2023-09-11 14:11:37 -0300

    extra

--- src/components/ShopingItem.jsx
+++ src/components/ShopingItem.jsx
@@ -2,29 +2,29 @@
 
 const ShoppingItem = (props) => {
     return (
-                    <div className="row d-flex justify-content-center mt-100S"> 
-                        <div className="col">                           
-                           <div className="card">
-                               <ul className="list-style-none">
-                                   <li className="d-flex no-block card-body">
-                                       <i className="fa fa-check-circle w-30px m-t-5"></i>
-                                       <div className="text-start">
-                                           <a href="#" className="m-b-0 font-medium p-0 text-start" data-abc="true">{props.name}</a>
-                                           <span className="text-muted text-start">Tarea N°{props.posicion + 1} </span>
-                                       </div>
-                                       <div className="position-absolute top-0 start-100">
-                                           <div className="tetx-right position-relative">
-                                           <button type="button" className="text-end position-relative btn btn-light" onClick={props.deleteItem}>
-                                            X  
-                                         </button>
-                                           </div>
-                                       </div>
-                                   </li>
-                                   </ul>
+        <div className="row d-flex justify-content-center mt-100S">
+            <div className="col">
+                <div className="card">
+                    <ul className="list-style-none">
+                        <li className="d-flex no-block card-body">
+                            <i className="fa fa-check-circle w-30px m-t-5"></i>
+                            <div className="text-start">
+                                <a href="#" className="m-b-0 font-large p-0 text-start" data-abc="true">{props.name}</a>
+                                <span className="text-muted text-start">Tarea N°{props.posicion + 1} </span>
+                            </div>
+                            <div className="position-absolute top-0 start-100">
+                                <div className="tetx-right position-relative">
+                                    <button type="button" className="text-end position-relative btn btn-dark" onClick={props.deleteItem}>
+                                        Delete
+                                    </button>
                                 </div>
                             </div>
-                        </div>    
-                        
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+
     )
 }
 
